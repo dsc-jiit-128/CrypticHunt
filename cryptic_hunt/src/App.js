c941289cfe2a4dd0999d28c3d806005f7f66be02
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2023-04-10 16:02:33 +0530

    home page created

---
+++ cryptic_hunt/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Home from './Pages/home';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
