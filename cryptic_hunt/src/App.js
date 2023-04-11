ac804604b93c6ad26c9f306b5bf75d79f685221a
TanayK07 <tkedia7@gmail.com>
2023-04-11 11:27:33 +0530

    Sad work bad pages

--- cryptic_hunt/src/App.js
+++ cryptic_hunt/src/App.js
@@ -12,10 +12,58 @@ import Register from './Pages/Register';
 import Team from './Pages/NewTeam';
 import Question from './Pages/Question1';
 import Question2 from './Pages/Q2';
+import Question3 from './Pages/Q3';
+import Question4 from './Pages/Q4';
+import Question5 from './Pages/Q5';
+import Question6 from './Pages/Q6';
+import Question7 from './Pages/Q7';
+import Question8 from './Pages/Q8';
+import Question9 from './Pages/Q9';
+import Question10 from './Pages/Q10';
 import Leaderboard from './Pages/Leaderboard';
 const Q2= () => {
   if (window.location.pathname === "/Q2") {
     return <Question2 />
+  }
+}
+const Q3= () => {
+  if (window.location.pathname === "/Q3") {
+    return <Question3 />
+  }
+}
+const Q4= () => {
+  if (window.location.pathname === "/Q4") {
+    return <Question4 />
+  }
+}
+const Q5= () => {
+  if (window.location.pathname === "/Q5") {
+    return <Question5 />
+  }
+}
+const Q6= () => {
+  if (window.location.pathname === "/Q6") {
+    return <Question3 />
+  }
+}
+const Q7= () => {
+  if (window.location.pathname === "/Q7") {
+    return <Question3 />
+  }
+}
+const Q8= () => {
+  if (window.location.pathname === "/Q8") {
+    return <Question3 />
+  }
+}
+const Q9= () => {
+  if (window.location.pathname === "/Q9") {
+    return <Question3 />
+  }
+}
+const Q10= () => {
+  if (window.location.pathname === "/Q10") {
+    return <Question10 />
   }
 }
 
@@ -25,6 +73,14 @@ function App() {
     <ChakraProvider>
 
     {Q2()}
+    {Q3()}
+    {Q4()}
+    {Q5()}
+    {Q6()}
+    {Q7()}
+    {Q8()}
+    {Q9()}
+    {Q10()}
       
      {
 
